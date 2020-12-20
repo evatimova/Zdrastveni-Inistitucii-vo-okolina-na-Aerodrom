8362c3f674f025e82ab290835cfed986173ef958
DejanMarkoski <74320371+DejanMarkoski@users.noreply.github.com>
2020-12-20 19:56:54 +0100

    Add files via upload

---
+++ Homework_3/HospitalFind88/Models/HospitalsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalFind88.Models
+{
+    public class HospitalsContext : DbContext
+    {
+        public DbSet<Hospitals> hospitals { get; set; }
+
+        public HospitalsContext() : base("DefaultConnection") {}
+
+        public static HospitalsContext Create()
+        {
+            return new HospitalsContext();
+        }
+    }
+}
