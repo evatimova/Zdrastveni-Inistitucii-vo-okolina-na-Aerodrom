b09c519dd39298d5e7781b0ebc75c907175d9a12
Младеновски Методи <metodi.mladenovski.1@students.finki.ukim.mk>
2021-01-31 02:03:57 +0100

    commit

---
+++ HospitalFind88/Models/Locations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospitalFind88.Models
+{
+    public class Locations
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter hospital name")]
+        [Display(Name = "Hospital Name")]
+        public string Name { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Emergency { get; set; }
+
+        public string Website { get; set; }
+
+        public string Street { get; set; }
+
+        public int Postcode { get; set; }
+
+
+        public virtual ICollection<Hospitals> hospitals { get; set; }
+
+       
+    }
+}
